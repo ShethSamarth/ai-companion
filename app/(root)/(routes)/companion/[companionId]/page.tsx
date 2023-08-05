918f261f5eca466d0ebd85a9f6fdfa1aa9f47971
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-08-05 21:24:44 +0530

    chat ui

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -11,10 +11,16 @@ interface CompanionIdPageProps {
 }
 
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirectToSignIn()
+  }
+
   // TODO: Check subscriptions
 
   const companion = await prismadb.companion.findUnique({
-    where: { id: params.companionId },
+    where: { id: params.companionId, userId },
   })
 
   const categories = await prismadb.category.findMany()
