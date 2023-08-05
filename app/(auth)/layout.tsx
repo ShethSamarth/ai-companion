2077d6a61e1319040bfd41326d0cdd6c0b308a1d
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-08-05 18:34:23 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex justify-center items-center h-full">{children}</main>
+  )
+}
+
+export default AuthLayout
